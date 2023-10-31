23a5dc89495da7e5eaebd34c3c79a152da17980b
Prashant <prashantchandrakar03@gmail.com>
2023-10-31 19:34:26 +0530

    first commit

---
+++ public/js/index.js
@@ -0,0 +1,25 @@
+
+const weatherForm = document.querySelector("form");
+const search = document.querySelector("input");
+const msgOne = document.querySelector("#msg-1");
+const msgTwo = document.querySelector("#msg-2");
+
+weatherForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const location = search.value;
+  msgOne.textContent = "Loading...";
+  msgTwo.textContent = "";
+  fetch("/weather?address=" + location).then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        msgOne.textContent = data.error;
+        msgTwo.textContent = "";
+      } else {
+        msgOne.textContent = data.location;
+        msgTwo.textContent = data.forecast;
+      }
+    });
+  });
+
+});
